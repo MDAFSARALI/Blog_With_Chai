cf62ab8fa5949c298fe2b5e486602061ffd394f5
MDAFSARALI <generaluserknown@gmail.com>
2025-06-26 15:47:37 +0530

    Finnaly 404

--- src/main.jsx
+++ src/main.jsx
@@ -74,7 +74,7 @@ const router = createBrowserRouter([
             element: <Post />,
         },
         {
-            path: "*",
+            path: "/*",
             element: <NotFound />,
         }
     ],
